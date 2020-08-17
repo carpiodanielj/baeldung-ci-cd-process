2b91241ca06a1a59b5aba0c1a39d42bc8e07ef74
Daniel Carpio <daniel.carpio@localmachine.net>
2020-08-17 13:12:50 -0500

    Added some configurations

--- src/main/java/com/example/demo/CiCdApplication.java
+++ src/main/java/com/example/demo/CiCdApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CiCdApplication {
 
 	/**
-	 * The main method
+	 * The main method.
 	 * @param args
 	 */
 	public static void main(String[] args) {
